1fd9c2e076061f0e6aad8f4af64b91d83fcecec4
gangchill <sabbir.ahmmed@gmail.com>
2016-10-31 21:55:56 +0100

    mM task

--- mM_task.py
+++ mM_task.py
@@ -1,4 +1,4 @@
-import sys
+
 with open('KLSadd.tex', "r") as old, open('KLSadd_New.tex', "w") as new:
     for line in old:
         words = []
@@ -16,30 +16,3 @@ with open('KLSadd.tex', "r") as old, open('KLSadd_New.tex', "w") as new:
             words.append(''.join(letters))
         print 'do mM swap :', ''.join(words)
         new.write(''.join(words))
-#
-# f = open('KLSadd.tex', "r")
-# line = f.readline()
-# while line:
-#     print 'before mM swap :', line
-#     list1 = []
-#     for letter in line:
-#         if letter == 'm' or letter == 'M':
-#             letter = letter.swapcase()
-#         else:
-#             pass
-#         list1.append(letter)
-#     print 'after mM swap :', ''.join(list1)
-#     line = f.readline()
-# f.close()
-# print "Finished!"
-#
-#
-#
-# def print_words(file_name):
-#     input_file = open(file_name, 'r')
-#     lines = input_file.readlines()
-#     lines = ' '.join(x for x in lines)
-#     print(lines)
-#
-# if __name__ == '__main__':
-#     print_words(sys.argv[1])
